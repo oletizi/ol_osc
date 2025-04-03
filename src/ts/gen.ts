e88abec4cca3d9ec77f7fae94a8d1679951dea6b
orion <oletizi@mac.com>
2025-04-03 11:59:46 -0700

    - generating widgets from spec

--- src/ts/gen.ts
+++ src/ts/gen.ts
@@ -1,6 +1,7 @@
 export interface Widget {
     type: string
     id: string
+    value: string
     top: number
     left: number
 }
@@ -28,4 +29,22 @@ export interface Spec {
 
 export function hydrateSpec(json: string): Spec {
     return JSON.parse(json) as Spec
+}
+
+export function genWidgets(device: Device) {
+    const rv: Widget[] = []
+    let top = 0, left = 0
+    for (const p of device.parameters) {
+        const fader: Widget = {
+            id: p.name, left: left, top: top, type: 'fader', value: '0'
+        }
+        rv.push(fader)
+        const label: Widget = {
+            id: `l_${p.name}`, left: left, top: 150, type: 'text', value: p.label
+        }
+        rv.push(label)
+
+        left += 50
+    }
+    return rv
 }
--- test/unit/gen.test.ts
+++ test/unit/gen.test.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs/promises'
 import path from 'node:path'
-import {hydrateSpec, hydrateWidgets} from '@/gen.js'
+import {genWidgets, hydrateSpec, hydrateWidgets} from '@/gen.js'
 import {expect} from 'chai'
 
 describe('gen basics', async () => {
@@ -28,15 +28,46 @@ describe('gen basics', async () => {
         if (device) {
             expect(device.name).to.equal('Test Device')
             expect(device.parameters).to.exist
-            expect(device.parameters.length).gte(1)
-            const [parameter] = device.parameters
-            expect(parameter).to.exist
-            if (parameter) {
-                expect(parameter.name).to.equal('Basic Parameter')
-                expect(parameter.type).to.equal('fader')
-                expect(parameter.osc).to.equal('/basic')
-                expect(parameter.label).to.equal('basic')
+            expect(device.parameters.length).gte(2)
+            const [p1] = device.parameters
+            expect(p1).to.exist
+            if (p1) {
+                expect(p1.name).to.equal('Param 1')
+                expect(p1.type).to.equal('fader')
+                expect(p1.osc).to.equal('/param1')
+                expect(p1.label).to.equal('param 1')
             }
+        }
+    })
+    it('generates widgets', async () => {
+        const widgetWidth = 50
+        const faderHeight = 150
+        const spec = hydrateSpec((await fs.readFile(path.join('test', 'data', 'spec.json'))).toString())
+        expect(spec.devices.length).gte(1)
+        if (spec.devices[0]) {
+            const widgets = genWidgets(spec.devices[0])
+            expect(widgets).to.exist
+            expect(widgets.length).gte(4)
+            const [f1, l1, f2, l2] = widgets
+
+            expect(f1).to.exist
+            expect(f1?.type).eq('fader')
+            expect(f1?.top).eq(0)
+            expect(f1?.left).eq(0)
+
+            expect(l1).to.exist
+            expect(l1?.type).eq('text')
+            expect(l1?.value).eq('param 1')
+            expect(l1?.top).eq(faderHeight)
+            expect(l1?.left).eq(0)
+
+            expect(f2).to.exist
+            expect(f2?.top).eq(0)
+            expect(f2?.left).eq(widgetWidth)
+
+            expect(l2).to.exist
+            expect(l2?.left).eq(widgetWidth)
+            expect(l2?.top).eq(faderHeight)
         }
     })
 })
