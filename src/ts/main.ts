732bc4e155f6d010e2b5d5f4d813529fb3c9d286
orion <oletizi@mac.com>
2025-04-03 22:04:18 -0700

    - generates osc document

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,7 @@
 import {Command} from 'commander'
 import {genWidgets, hydrateSpec} from '@/gen.js'
 import * as fs from 'fs/promises'
+import {newConfig} from '@/config.js'
 
 const program = new Command()
 program
@@ -11,9 +12,11 @@ program
 program.parse(process.argv)
 
 async function doSpec(filePath: string) {
+    const config = await newConfig()
     const spec = hydrateSpec((await fs.readFile(filePath)).toString())
     for (const device of spec.devices) {
-        console.log(device.name)
-        console.log(genWidgets(device))
+        const doc = config.newOscDocumentTemplate()
+        doc.content.widgets = genWidgets(config, device)
+        console.log(JSON.stringify(doc, null, 2))
     }
 }
