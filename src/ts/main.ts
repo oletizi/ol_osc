e584475c0e8633207f3c49b8d01d79a07d0400d2
orion <oletizi@mac.com>
2025-04-03 21:13:07 -0700

    - generating OSC widgets

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,6 @@
 import {Command} from 'commander'
+import {genWidgets, hydrateSpec} from '@/gen.js'
+import * as fs from 'fs/promises'
 
 const program = new Command()
 program
@@ -9,5 +11,9 @@ program
 program.parse(process.argv)
 
 async function doSpec(filePath: string) {
-    console.log(`Spec path: ${filePath}`)
+    const spec = hydrateSpec((await fs.readFile(filePath)).toString())
+    for (const device of spec.devices) {
+        console.log(device.name)
+        console.log(genWidgets(device))
+    }
 }
