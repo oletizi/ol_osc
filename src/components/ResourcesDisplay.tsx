eaaa2cee63b4d16a8f0aa8933ed36ee32225378d
orion <oletizi@mac.com>
2025-04-29 22:28:21 -0700

    - checkbox

--- src/components/ResourcesDisplay.tsx
+++ src/components/ResourcesDisplay.tsx
@@ -11,6 +11,7 @@ import {useEffect, useState} from 'react'
 import type {Config} from '@/config.ts'
 import {newClient} from '@/plughost-client.ts'
 import {Button} from '@/components/ui/button.tsx'
+import { Checkbox } from '@/components/ui/checkbox'
 
 export function ResourcesDisplay({endpoint}: { endpoint: URL }) {
     const [config, setConfig] = useState<Config | null>()
@@ -34,8 +35,8 @@ export function ResourcesDisplay({endpoint}: { endpoint: URL }) {
             </CardHeader>
             <CardContent>
                 {config ?
-                    <ul className="radius border-1 border-secondary shadow-inner max-h-50 overflow-auto">{config.hostConfig.availableResources.plugins.map(p => (
-                        <li className="text-sm px-4 py-1 hover:bg-secondary cursor-default">{p.name}</li>))}</ul> : ''}
+                    <ul className="radius pt-1 border-1 border-secondary shadow-inner max-h-50 overflow-auto">{config.hostConfig.availableResources.plugins.map(p => (
+                        <li className="text-sm pl-2 pr-4 py-1 hover:bg-secondary cursor-default flex gap-2"><Checkbox/> {p.name}</li>))}</ul> : ''}
             </CardContent>
             <CardFooter><Button>Apply</Button></CardFooter>
         </Card>
