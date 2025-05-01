ab38e82a057830259df940388e1eafc6f7529a2c
orion <oletizi@mac.com>
2025-04-30 22:03:49 -0700

    - integration test for baking out web app config to plughost config

--- src/ts/plughost.ts
+++ src/ts/plughost.ts
@@ -1,9 +1,25 @@
 import {type Config, saveConfig} from '@/config.ts'
 import {execute} from '@oletizi/lib-runtime'
 import type {Device} from '@/model.ts'
+import path from 'path'
+import * as fs from 'fs/promises'
 
-export async function bakePlughostConfig(config:Config) {
-
+export async function bakePlughostConfig(config: Config) {
+    const outfile = path.join(config.dataDir, 'config')
+    await fs.writeFile(outfile, '')
+    for (const device of config.hostConfig.activePluginChain) {
+        // Plugin: <Format: AudioUnit>, <Name: HRTFPanner>
+        // Plugin Parameter: <Format: AudioUnit>, <Plugin Name: HRTFPanner>, <Parameter Name: gain>
+        // Plugin Parameter: <Format: AudioUnit>, <Plugin Name: HRTFPanner>, <Parameter Name: azimuth>
+        // Plugin Parameter: <Format: AudioUnit>, <Plugin Name: HRTFPanner>, <Parameter Name: elevation>
+        // Plugin Parameter: <Format: AudioUnit>, <Plugin Name: HRTFPanner>, <Parameter Name: distance>
+        // Plugin Parameter: <Format: AudioUnit>, <Plugin Name: HRTFPanner>, <Parameter Name: coordinate scale>
+        // Plugin Parameter: <Format: AudioUnit>, <Plugin Name: HRTFPanner>, <Parameter Name: reference distance>
+        await fs.appendFile(outfile, `Plugin: <Format: ${device.type}>, <Name: ${device.name}>\n`)
+        for (const param of device.parameters) {
+            await fs.appendFile(outfile, `Plugin Parameter: <Format: ${device.type}>, <Plugin Name: ${param.name}>\n`)
+        }
+    }
 }
 
 export async function updateAvailableResources(config: Config): Promise<Config> {
--- test/integration/plughost.test.ts
+++ test/integration/plughost.test.ts
@@ -1,6 +1,9 @@
-import {updateAvailableResources} from '@/plughost.ts'
+import {bakePlughostConfig, updateAvailableResources} from '@/plughost.ts'
+import tmp from 'tmp'
 import {expect} from 'chai'
 import {newConfig} from '@/config.ts'
+import path from 'path'
+import * as fs from 'node:fs'
 
 describe('plughost', async () => {
     it('updates available resources in the local environment', async function () {
@@ -14,7 +17,18 @@ describe('plughost', async () => {
 
     it('bakes out plughost config based on web app config', async function () {
         this.timeout(30 * 1000)
-        const config = await newConfig()
-        console.log(`config`, config)
+        const dataDir = tmp.dirSync().name
+        expect(fs.existsSync(dataDir))
+        for (const name of ['available.json', 'host.json']) {
+            const testData = path.join('test', 'data', 'config', name)
+            fs.cpSync(testData, path.join(dataDir, name))
+        }
+        console.log('dataDir', dataDir)
+        const config = await newConfig(dataDir)
+        await bakePlughostConfig(config)
+        const configFile = path.join(dataDir, 'config')
+        expect(fs.existsSync(configFile)).to.be.true
+
+        console.log(String(fs.readFileSync(configFile)))
     })
 })
